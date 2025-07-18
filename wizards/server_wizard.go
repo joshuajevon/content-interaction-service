4b17c2594bd3909a85d2c94b17af159bfcca7b44
Jordan Jeferson Juliman <>
2025-07-18 11:32:52 +0700

    dislike Post

--- wizards/server_wizard.go
+++ wizards/server_wizard.go
@@ -9,5 +9,6 @@ func RegisterServer(router *gin.Engine) {
 	post := router.Group("/posts")
 	{
 		post.POST("/view/likes/:id", LikesHttp.LikePost)
+		post.POST("/view/dislikes/:id", LikesHttp.DislikePost)
 	}
 }
